a475a0e7cb2d19ff9791e2734de2afbc1a03c8e7
nicolasMachut <machut.nicolas@live.fr>
2015-09-12 17:33:08 +0200

    Add gameplay && multiplayer

---
+++ Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkController : MonoBehaviour {
+
+    private string gameVersion = "0.1";
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(gameVersion);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnJoinedLobby()
+    {
+        print("**************************************************************");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        print("** create room **");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        print("On vient de rejoindre une room");
+        PhotonNetwork.Instantiate("Player", player.transform.position, Quaternion.identity, 0);
+    }
+}
